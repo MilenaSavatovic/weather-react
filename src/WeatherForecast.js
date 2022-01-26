6cd7d379b6b46856bed73add3cc87449eb1d6c5a
MilenaSavatovic <96110742+MilenaSavatovic@users.noreply.github.com>
2022-01-26 14:18:42 +0100

    Added useEffect

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './WeatherForecast.css'
 import axios from 'axios'
 
@@ -7,6 +7,10 @@ import WeatherForecastDay from './WeatherForecastDay'
 export default function WeatherForecast(props) {
   const [loaded, setLoaded] = useState(false)
   let [forecast, setForecast] = useState(null)
+
+  useEffect(() => {
+    setLoaded(false)
+  }, [props.coords])
 
   function handleResponse(response) {
     setForecast(response.data.daily)
@@ -17,9 +21,17 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          <div className="col">
-            <WeatherForecastDay data={forecast[0]} />
-          </div>
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col">
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              )
+            } else {
+              return null
+            }
+          })}
         </div>
       </div>
     )
--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import WeatherIcon from './WeatherIcon'
+import './WeatherForecastDay.css'
 
 export default function WeatherForecastDay(props) {
   let maxTemp = Math.round(props.data.temp.max)
