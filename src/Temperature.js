82e69a1d7a59408e5415d72e436d70c5001f840d
MilenaSavatovic <96110742+MilenaSavatovic@users.noreply.github.com>
2022-01-27 13:35:50 +0100

    Removed fahrenheit conversion

--- src/Temperature.js
+++ src/Temperature.js
@@ -1,46 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './temperature.css'
 
 export default function Temperature(props) {
-  const [unit, setUnit] = useState('celsius')
-
-  function convertToFahrenheit(event) {
-    event.preventDefault()
-    setUnit('fahrenheit')
-  }
-
-  function showCelsius(event) {
-    event.preventDefault()
-    setUnit('celsius')
-  }
-
-  function fahrenheit() {
-    return (props.temperature * 9) / 5 + 32
-  }
-
-  if (unit === 'celsius') {
-    return (
-      <span className="float-left data">
-        <strong> {Math.round(props.temperature)}</strong>
-        <span className="units">
-          °C |{' '}
-          <a href="/" onClick={convertToFahrenheit} className="active">
-            °F
-          </a>
-        </span>
-      </span>
-    )
-  } else {
-    return (
-      <span className="float-left data">
-        <strong> {Math.round(fahrenheit())}</strong>
-        <span className="units">
-          <a href="/" onClick={showCelsius} className="active">
-            °C
-          </a>
-          | °F
-        </span>
-      </span>
-    )
-  }
+  return (
+    <span className="float-left data">
+      <strong> {Math.round(props.temperature)}</strong>
+      <span className="units">°C</span>
+    </span>
+  )
 }
